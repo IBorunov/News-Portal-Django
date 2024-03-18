84a9e675d0a8e1cdc0ab06bfca26f51c21b803f9
Ivan Borunov <145274717+IBorunov@users.noreply.github.com>
2024-03-18 16:34:06 +0300

    Add files via upload

--- News_Portal/news/templatetags/custom_filters.py
+++ News_Portal/news/templatetags/custom_filters.py
@@ -10,32 +10,32 @@ def censor(value):
                 'ваыупап',
                 'нпосн',
                 'папркерщж',
-                ]  # гадости
-    length = len(yuck)  # записываем кол-во гадостей
-    filtered_text = ''  # отфильтрованный текст
-    string = ''  # переменная, где будем хранить проверяемую строку
-    pattern = '*'  # чем заменять гадости
-    for i in value:  # проходимся циклом по тексту
-        string += i  # записываем полученные строки в переменную
-        string2 = string.lower()  # приводим строки к нижн. регистру
+                ]
+    length = len(yuck)
+    filtered_text = ''
+    string = ''
+    pattern = '*'
+    for i in value:
+        string += i
+        string2 = string.lower()
 
-        clear = 0  # "чистые строки"
-        for j in yuck:  # ищем гадости в тексте
-            if not string2 in j:  # если не нашли гадость:
-                clear += 1  # запоминаем и ищем следующее плохое слово
-            if string2 == j:  # а если нашли:
-                filtered_text += pattern * len(string)  # заменяем его на звездочки и записываем в отфильтрованные
+        clear = 0
+        for j in yuck:
+            if not string2 in j:
+                clear += 1
+            if string2 == j:
+                filtered_text += pattern * len(string)
                 clear -= 1
-                string = ''  # обнуляем строку
+                string = ''
 
-        if clear == length:  # после того как просмотрели все варианты гадостей:
-            filtered_text += string  # добавляем строку в отфильтрованный текст
-            string = ''  # обнуляем значение переменной со строкой и проверяем дальше
+        if clear == length:
+            filtered_text += string
+            string = ''
 
     if string2 != '' and string2 not in yuck:
-        filtered_text += string #если гадости не встретились, записываем эту строку
+        filtered_text += string
     elif string2 != '':
-        filtered_text += pattern * len(string) #иначе заменяем плохие слова на звездочки
+        filtered_text += pattern * len(string)
 
     return filtered_text
 
